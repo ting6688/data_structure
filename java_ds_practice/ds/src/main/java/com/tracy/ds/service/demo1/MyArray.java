a5ed33930b43f30767c9404bad7b4d04531ed7e0
zhangting <1510079209@qq.com>
2021-10-30 14:40:04 +0800

    feat:获取指定位置的元素

--- java_ds_practice/ds/src/main/java/com/tracy/ds/service/demo1/MyArray.java
+++ java_ds_practice/ds/src/main/java/com/tracy/ds/service/demo1/MyArray.java
@@ -1,5 +1,7 @@
 package com.tracy.ds.service.demo1;
 
+import java.util.Arrays;
+
 public class MyArray {
 
     //目标数组，用于存储数据的数组
@@ -15,9 +17,9 @@ public class MyArray {
     }
 
     //往数组的末尾添加一个元素
-    public void add(int element){
+    public void add(int element) {
         // 创建一个新的数组
-        int[] newArr = new int[elements.length+1];
+        int[] newArr = new int[elements.length + 1];
         // 把原数组中的元素复制到新数组中---循环原数组，因为数组长度以原数组的长度为主
 //        // error: java.lang.ArrayIndexOutOfBoundsException: 0
 //        for (int i : newArr) {
@@ -33,19 +35,56 @@ public class MyArray {
 
         // ✅
         for (int i = 0; i < elements.length; i++) {
-            System.out.println("i:"+i);
+            System.out.println("i:" + i);
             newArr[i] = elements[i];
         }
 
         // error: newArr 长度非0 而elements长度为0 elements[0] 会报数组索引越界的问题
-        for (int i : newArr) {
-            System.out.println("i:"+i);
-            newArr[i] = elements[i];
-        }
+//        for (int i : newArr) {
+//            System.out.println("i:"+i);
+//            newArr[i] = elements[i];
+//        }
 
         // 把添加的元素放入新数组中
-        newArr[newArr.length-1] = element;
+        newArr[newArr.length - 1] = element;
         // 新数组替换掉原数组
         elements = newArr;
     }
+
+    // 打印所有元素到控制台
+    public void show() {
+        System.out.println(Arrays.toString(elements));
+    }
+
+    // 删除数组中的元素
+    public void delete(int index) {
+        //判断下标是否越界
+        if (index < 0 || index > elements.length - 1) {
+            throw new RuntimeException("下标越界");
+        }
+        // 创建一个新的数组，长度为原数组的长度-1
+        int[] newArr = new int[elements.length - 1];
+        // 复制原有数据到新数组
+        for (int i = 0; i < newArr.length; i++) {
+            // 想要删除的元素的前面的元素，相同位置上值一样
+            if (i < index) {
+                newArr[i] = elements[i];
+            } else {
+                // 想要删除的后面的元素，整体往前挪动一位
+                newArr[i] = elements[i + 1];
+            }
+        }
+        // 新数组替换掉原数组
+        elements = newArr;
+    }
+
+    // 取出指定位置的元素
+    public int get(int index) {
+        //判断下标是否越界
+        if (index < 0 || index > elements.length - 1) {
+            throw new RuntimeException("下标越界");
+        }
+        return elements[index];
+    }
+
 }
--- java_ds_practice/ds/src/main/java/com/tracy/ds/service/demo1/TestMyArray.java
+++ java_ds_practice/ds/src/main/java/com/tracy/ds/service/demo1/TestMyArray.java
@@ -3,13 +3,25 @@ package com.tracy.ds.service.demo1;
 public class TestMyArray {
 
     public static void main(String[] args) {
-        //创建一个可变的数组
+        //创建一个可变的数组 []
         MyArray myArray = new MyArray();
+        myArray.show();
+
         //获取长度
         int size = myArray.size();
-        System.out.println("size:"+ size);
-        // 往可变数组中添加一个元素
+        System.out.println("size:" + size);
+        // 往可变数组中添加一个元素 [99, 98]
         myArray.add(99);
-        System.out.println("size:"+myArray.size());
+        myArray.add(98);
+        myArray.add(97);
+        System.out.println("size:" + myArray.size());
+        // 显示可变数组中的所有元素到控制台
+        myArray.show();
+        // 删除某个元素
+        myArray.delete(1);
+        myArray.show();
+        // 2
+        System.out.println(myArray.size());
+        System.out.println(myArray.get(9));
     }
 }
