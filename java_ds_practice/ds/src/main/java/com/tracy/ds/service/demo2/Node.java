b45b344d88d3bc2000e9a98dba956b6f64ddb384
zhangting <1510079209@qq.com>
2021-11-03 08:15:49 +0800

    feat:单链表的添加元素

---
+++ java_ds_practice/ds/src/main/java/com/tracy/ds/service/demo2/Node.java
@@ -0,0 +1,51 @@
+package com.tracy.ds.service.demo2;
+
+
+/**
+ * 单链表 结构：数据+下一个结点
+ */
+public class Node {
+
+    public int data;
+
+    public Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    /**
+     * 单链表的追加
+     *
+     * @param node
+     */
+    public void append(Node node) {
+        this.next = node;
+    }
+
+    /**
+     * 获取节点数据
+     *
+     * @param node
+     * @return
+     */
+    public int getData() {
+        return data;
+    }
+
+    /**
+     * 追加节点的升级 todo 待测试
+     *
+     * @param node
+     */
+    public void append_ext(Node node) {
+        // 当前节点
+        Node currentNode = this;
+        // 寻找当前节点的下一节点，当不为空时，继续寻找，当为空时，将目标节点追加在其后面
+        Node nextNode = currentNode.next;
+        while (nextNode != null) {
+            currentNode = nextNode;
+        }
+        currentNode.next = node;
+    }
+}
---
+++ java_ds_practice/ds/src/main/java/com/tracy/ds/service/demo2/TestMyNode.java
@@ -0,0 +1,28 @@
+package com.tracy.ds.service.demo2;
+
+public class TestMyNode {
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        System.out.println(node1.getData());
+        System.out.println(node2.getData());
+        System.out.println(node3.getData());
+        // node节点的追加方式 不灵活 ，有没有一种方式实现一个节点一直往后追加，被追加的节点挂在上一节点的后面，就像火车一样
+        node1.append(node2);
+        node2.append(node3);
+        // 2
+        System.out.println(node1.next.getData());
+        // 3
+        System.out.println(node1.next.next.getData());
+
+
+
+
+
+
+
+
+    }
+}
