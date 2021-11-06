2f8621a7f0f6cbdc3dac838d6055bd228a63971a
zhangting <1510079209@qq.com>
2021-11-06 12:37:18 +0800

    feat:单链表的添加元素的优化

--- java_ds_practice/ds/src/main/java/com/tracy/ds/service/demo2/Node.java
+++ java_ds_practice/ds/src/main/java/com/tracy/ds/service/demo2/Node.java
@@ -34,18 +34,31 @@ public class Node {
     }
 
     /**
-     * 追加节点的升级 todo 待测试
+     * 追加节点的升级
      *
      * @param node
      */
-    public void append_ext(Node node) {
+    public Node append_ext(Node node) {
         // 当前节点
         Node currentNode = this;
+        System.out.println("currentNode:"+ currentNode.getData());
         // 寻找当前节点的下一节点，当不为空时，继续寻找，当为空时，将目标节点追加在其后面
         Node nextNode = currentNode.next;
         while (nextNode != null) {
             currentNode = nextNode;
+            // 没有下边这一句时，程序是个死循环
+            nextNode = currentNode.next;
         }
         currentNode.next = node;
+        System.out.println("nextNode:"+ currentNode.next.getData());
+        return currentNode;
     }
+
+    /**
+     * 获取下一节点
+     */
+//    public Node next(){
+//        return this.
+//    }
+
 }
--- java_ds_practice/ds/src/main/java/com/tracy/ds/service/demo2/TestMyNode.java
+++ java_ds_practice/ds/src/main/java/com/tracy/ds/service/demo2/TestMyNode.java
@@ -6,16 +6,30 @@ public class TestMyNode {
         Node node1 = new Node(1);
         Node node2 = new Node(2);
         Node node3 = new Node(3);
-        System.out.println(node1.getData());
-        System.out.println(node2.getData());
-        System.out.println(node3.getData());
-        // node节点的追加方式 不灵活 ，有没有一种方式实现一个节点一直往后追加，被追加的节点挂在上一节点的后面，就像火车一样
-        node1.append(node2);
-        node2.append(node3);
-        // 2
+//        System.out.println(node1.getData());
+//        System.out.println(node2.getData());
+//        System.out.println(node3.getData());
+//        // node节点的追加方式 不灵活 ，有没有一种方式实现一个节点一直往后追加，被追加的节点挂在上一节点的后面，就像火车一样
+//        node1.append(node2);
+//        node2.append(node3);
+//        // 2
+//        System.out.println(node1.next.getData());
+//        // 3
+//        System.out.println(node1.next.next.getData());
+
+//        node1.append_ext(node2);
+//        node2.append_ext(node3);
+//        // 2
+//        System.out.println(node1.next.getData());
+//        // 3
+//        System.out.println(node1.next.next.getData());
+
+        node1.append_ext(node2).append_ext(node3);
+//        node2.append_ext(node3);
         System.out.println(node1.next.getData());
         // 3
         System.out.println(node1.next.next.getData());
+
 
 
 
